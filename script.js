456e8b3bab4bb1e7e7323dbe0cb1a569f5bd1cca
Hagr Saad <hagrsaad16@gmail.com>
2023-07-20 13:35:24 -0400

    Add display library and book functions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = []; 
+let container = document.querySelector('.container');
 
 function Book(title, author, pages, isRead){
     this.title = title,
@@ -16,8 +17,41 @@ function addBookToLibrary(book){
     myLibrary.push(book);
 }
 
+function displayBookDetails(book, bookDiv){
+    let titleHTML = document.createElement('h2');
+    titleHTML.innerHTML = book.title;
+
+    let authorHTML = document.createElement('p');
+    authorHTML.innerHTML = book.author;
+
+    let pagesHTML = document.createElement('p');
+    pagesHTML.innerHTML = book.pages + " pages";
+
+    let isReadHTML = document.createElement('p');
+    isReadHTML.innerHTML = (book.isRead ? "has been read" : "not read yet");
+
+    bookDiv.appendChild(titleHTML);
+    bookDiv.appendChild(authorHTML);
+    bookDiv.appendChild(pagesHTML);
+    bookDiv.appendChild(isReadHTML);
+}
+
+function displayBook(book){
+    let bookDiv = document.createElement('div');
+    bookDiv.classList.add("book");
+    displayBookDetails(book, bookDiv);
+    container.appendChild(bookDiv);
+}
+
+function displayLibrary(){
+    myLibrary.forEach(book => {
+        displayBook(book);
+    });
+}
+
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 const harryPotter = new Book("Harry Potter", "J.K. Rowling", 345, true);
 addBookToLibrary(theHobbit);
 addBookToLibrary(harryPotter);
 console.log(myLibrary);
+displayLibrary();
